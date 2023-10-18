12bbab56010131bdc474ab62a4620997918c4552
Diss <65290988+Dissurender@users.noreply.github.com>
2023-10-18 16:08:17 -0500

    redis config continued

--- db/index.js
+++ db/index.js
@@ -1,7 +1,14 @@
 const { createClient } = require('redis');
 
-const client = createClient();
+const client = createClient({
+  // url: `redis://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}`
+  url: 'redis://127.0.0.1:6379',
+});
 
-client.on('error', (err) => console.error('Redis Client bork bork...'));
+client.on('error', (err) => console.error('Redis Client bork bork...', err));
 
 await client.connect();
+
+if (client.isReady) {
+  console.log("Redis ready...")
+}
