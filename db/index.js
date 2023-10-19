a7ded6cbaaf0be2f0ed808e28e793aa679503eb8
diss <65290988+Dissurender@users.noreply.github.com>
2023-10-18 19:06:00 -0500

    Revert "pg added back in"
    This reverts commit 7d6098a1fb00e42d948c5de03e6519950cd94e99.

--- db/index.js
+++ db/index.js
@@ -1,17 +1,7 @@
-const { Pool } = require('pg');
-const config = require('./database.config.js')
+const { createClient } = require('redis');
 
-console.log('Init DB..');
+const client = createClient();
 
-const pool = new Pool(config.development);
+client.on('error', (err) => console.error('Redis Client bork bork...'));
 
-module.exports = {
-  query: async (text, params) => {
-    const initTime = Date.now();
-    const response = await pool.query(text, params);
-    const dur = Date.now() - initTime;
-    console.log('PG Query fired:', { text, dur, rows: response.rowCount });
-    return response;
-  },
-  pool,
-};
+await client.connect();
