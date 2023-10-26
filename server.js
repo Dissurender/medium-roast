be6a7c6152d6715e54c23c73acc1c269e98a0599
diss <65290988+Dissurender@users.noreply.github.com>
2023-10-26 03:45:37 -0500

    cron adjustment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 const app = express();
 import routes from './routes/index.js';
 import morgan from 'morgan';
+import { job } from './utils/cron.js';
 
 // import { requestLogger } from './middleware/logger.js';
 const port = process.env.PORT || 8000;
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 app.use(routes);
+
+// job.start()
 
 app.listen(port, () => {
   `Server is running on port: http://localhost:${port}/`;
--- utils/cron.js
+++ utils/cron.js
@@ -1,9 +1,9 @@
 import { getTopStories } from '../controllers/ingestController.js';
 import { CronJob } from 'cron';
-const job = new CronJob(
+
+export const job = new CronJob(
   '*/30 * * * *',
-  getTopStories(),
-  null,
-  true,
-  'America/Chicago'
+  function () {
+    getTopStories();
+  },
 );
