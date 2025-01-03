2e1290fe906ec44a8b8bcbb0a5071abd43929b7a
Rhyn Ogg <65290988+Dissurender@users.noreply.github.com>
2025-01-02 19:57:42 -0600

    jsdoc regen

--- routes/api.js
+++ routes/api.js
@@ -2,14 +2,14 @@ import express from 'express';
 const router = express.Router();
 import {
   getComment,
+  getRecentStories,
   getStory,
   getTopStories,
 } from '../controllers/storiesController.js';
-import { getMostRecentStory } from '../controllers/ingestController.js';
 
 router.get('/stories/top', getTopStories);
 
-router.get('/stories/fresh', getMostRecentStory);
+router.get('/stories/fresh', getRecentStories);
 router.use('/stories/:id', getStory);
 router.use('/comments/:id', getComment);
 
